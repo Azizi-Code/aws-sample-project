e39a198c4723e81a84dd34beb2be1a802fafbcdc
Roohallah Azizi <roohallah.azizi@virtudev.net>
2023-11-06 23:06:43 +0300

    feat: add simple publisher and consumer

---
+++ SampleSqs/SimpleMessagePublish.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SampleSqs;
+
+var sqsClient = new AmazonSQSClient();
+var queueUrl = await sqsClient.GetQueueUrlAsync("Client");
+
+var request = new SendMessageRequest
+{
+    QueueUrl = queueUrl.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(new { Message = "Hello World!" })
+};
+
+await sqsClient.SendMessageAsync(request);
+
+// Call consume method
+var test = new SimpleMessageConsume();
+await test.ConsumeMessageAsync();
